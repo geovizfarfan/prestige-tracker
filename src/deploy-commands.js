6339655bae0fc3cc2391b9453d94840999a874ec
Geovizfarfan <geovizfarfan@icloud.com>
2026-08-31 13:29:17 -0400

    Deploy commands globally

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -37,7 +37,7 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
   try {
     console.log(`Deploying ${commands.length} slash commands...`);
     await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands },
     );
     console.log('✅ Slash commands deployed!');
